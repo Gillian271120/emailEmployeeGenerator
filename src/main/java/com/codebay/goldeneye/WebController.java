5c9a6ba7879a32911afbc4fdb1cc59365887a805
unknown <Gillian271120@gmail.com>
2022-09-22 19:13:47 +0100

    ref#3 WIP, added offices

--- src/main/java/com/codebay/goldeneye/WebController.java
+++ src/main/java/com/codebay/goldeneye/WebController.java
@@ -1,5 +1,7 @@
 package com.codebay.goldeneye;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,25 @@ public class WebController {
 
     @GetMapping("/")
     public String index() {
+        
+        ArrayList<String> spain = new ArrayList<>(
+            Arrays.asList("Research & development", "Business")
+        );
+        ArrayList<String> milan = new ArrayList<>(
+            Arrays.asList("Design", "Business", "Advertising")
+        );
+        ArrayList<String> newYork = new ArrayList<>(
+            Arrays.asList("Business", "Advertising")
+        );
+        ArrayList<ArrayList<String>> departments = new ArrayList<>(Arrays.asList(spain, milan, newYork));
+        Offices.setDepartments(departments);
+        
+        //for (int i = 0; i < departments.size(); i++) {
+        //    System.out.println(departments.get(i));
+        //    for (int j = 0; j < departments.indexOf(i); j++) {
+        //        System.out.println(departments.get(i).get(j));
+        //    }
+        //
         return "index";
     }
 
